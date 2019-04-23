01a3e08d478d4811e5f509510dcf1e5be58062a2
juhahinkula <hinkula@gmail.com>
2019-04-23 09:07:39 +0300

    Initial commit

---
+++ src/main/java/fi/haagahelia/bookstore/web/BookstoreController.java
@@ -0,0 +1,16 @@
+package fi.haagahelia.bookstore.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@ResponseBody
+public class BookstoreController {
+
+	@GetMapping("/")
+	public String index() {
+		return "Hello";
+	}
+	
+}
